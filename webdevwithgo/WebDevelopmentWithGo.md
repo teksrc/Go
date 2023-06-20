8bd3d6f136413f08cf7ba3662e0b2d075f728264
TekSrc <Frankacarv@gmail.com>
2023-06-20 16:28:59 -0700

    Add main.go, modd.conf, & go.modd

--- webdevwithgo/WebDevelopmentWithGo.md
+++ webdevwithgo/WebDevelopmentWithGo.md
@@ -1,3 +1,6 @@
 # Web Development with Golang
 
 https://courses.calhoun.io/courses/cor_wdv2
+
+Common Causes and Fixes for Errors in Go
+https://errorsingo.com/compliation-no-new-variables-on-left-side-of/
--- webdevwithgo/lenslocked/main.go
+++ webdevwithgo/lenslocked/main.go
@@ -1,16 +1,20 @@
 package main
 
+// `go install github.com/cortesi/modd/cmd/modd@latest` for Modd at global level
+
 import (
 	"fmt"
 	"net/http"
+	// "os"
 )
 
 func handleFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to an awesome Go site!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to an awesome Go Web API!</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", handleFunc)
+	// fmt.Fprint(os.Stdout, "Hello World!")
 	fmt.Println("Starting server on http://localhost:3000...")
 	http.ListenAndServe(":3000", nil)
 }
