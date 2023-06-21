b0135fa7f3f84eb5cd56d50a1221f74b9e1506ab
TekSrc <Frankacarv@gmail.com>
2023-06-20 23:17:06 -0700

    Update main.go response Header

--- webdevwithgo/lenslocked/main.go
+++ webdevwithgo/lenslocked/main.go
@@ -9,6 +9,7 @@ import (
 )
 
 func handleFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8" )
 	fmt.Fprint(w, "<h1>Welcome to an awesome Go Web API!</h1>")
 }
 
