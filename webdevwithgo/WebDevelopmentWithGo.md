cc978cd6e8440c4360a788ce72d36851f7d288e6
TekSrc <Frankacarv@gmail.com>
2023-06-21 10:02:35 -0700

    Update main.go routing, update WebDevWithGo md

--- webdevwithgo/WebDevelopmentWithGo.md
+++ webdevwithgo/WebDevelopmentWithGo.md
@@ -12,3 +12,13 @@ https://pkg.go.dev/net/http
 https://pkg.go.dev/net/url#URL encoding.decoding raw path
 https://meyerweb.com/eric/tools/dencoder/ helpful
 https://pkg.go.dev/net/http#pkg-constants
+
+There are two main types in the net/http package:
+
+1. http.Handler - an interface with a ServeHTTP method
+2. http.HandlerFunc - a function that accepts the same args as a ServeHTTP method
+
+There are also two functions in the http package that look similar, and are used to register their respectively similar types for a web server:
+
+1. http.Handle - a function that accepts a pattern and an http.Handler as its arguments.
+2. http.HandleFunc - a function that accepts a pattern and a function that looks like a http.HandlerFunc as its arguments.
--- webdevwithgo/lenslocked/main.go
+++ webdevwithgo/lenslocked/main.go
@@ -5,27 +5,10 @@ import (
 	"net/http"
 )
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprint(w, r.URL.Path) // ability to get the path off the request
-	// fmt.Fprint(w, r.URL.Path)
-	// fmt.Fprint(w, r.URL.RawPath)
-
-	// use a switch instead
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		notFoundHandler(w, r)
-	}
-}
-
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8" )
 	fmt.Fprint(w, "<h1>Welcome to an awesome Go Web API!</h1>")
 }
-
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8" )
@@ -36,8 +19,49 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Page Not Found", http.StatusNotFound)
 }
 
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	// fmt.Fprint(w, r.URL.Path) // ability to get the path off the request
+// 	// fmt.Fprint(w, r.URL.Path)
+// 	// fmt.Fprint(w, r.URL.RawPath)
+
+// 	// use a switch instead
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		notFoundHandler(w, r)
+// 	}
+// }
+
+type Router struct {}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprint(w, r.URL.Path) // ability to get the path off the request
+	// fmt.Fprint(w, r.URL.Path)
+	// fmt.Fprint(w, r.URL.RawPath)
+
+	// use a switch instead of if else if
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		notFoundHandler(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", pathHandler)
+	var router Router
+	// var router http.HandlerFunc = pathHandler
+	// Why does Go allow both types and conversion, makes life easier for us to not have to convert over.
+	// http.Handler - interface with the ServeHTTP method
+	// http.HandlerFunc - function that accepts same args as ServeHTTP method, but implements http.Handler
+	// http.Handle - function that takes in a pattern and takes in an http.Handler
+	// http.HandleFunc - takes in a pattern and takes in a function that has underlying type looks like http.handlerFunc
+	// http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
 	fmt.Println("Starting server on http://localhost:3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
