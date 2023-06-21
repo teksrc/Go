f7293f1072e80b9a14b6874b1d3fd3e6f9844217
TekSrc <Frankacarv@gmail.com>
2023-06-20 23:30:22 -0700

    Add contactHandler, add contact page

--- webdevwithgo/lenslocked/main.go
+++ webdevwithgo/lenslocked/main.go
@@ -8,16 +8,22 @@ import (
 	// "os"
 )
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// Content Types: text/html, application/json, image/png, & video/mp4 for example.
 	// https://pkg.go.dev/net/http
 	w.Header().Set("Content-Type", "text/html; charset=utf-8" )
 	fmt.Fprint(w, "<h1>Welcome to an awesome Go Web API!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8" )
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:frankacarv@gmail.com\">frankacarv@gmail.com</a></p>")
+}
+
 func main() {
-	http.HandleFunc("/", handleFunc)
 	// fmt.Fprint(os.Stdout, "Hello World!")
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting server on http://localhost:3000...")
 	http.ListenAndServe(":3000", nil)
 }
