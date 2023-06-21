e6c497fb844bb4a9820cb502b58d647b09975827
TekSrc <Frankacarv@gmail.com>
2023-06-21 10:17:53 -0700

    Add faqHandler for faq page to router

--- webdevwithgo/lenslocked/main.go
+++ webdevwithgo/lenslocked/main.go
@@ -15,39 +15,40 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:frankacarv@gmail.com\">frankacarv@gmail.com</a></p>")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul>
+		<li>
+			<b>Is there a free version?</b>
+			Yes! We offer a free trial for 30 days on any paid plans.
+		</li>
+		<li>
+			<b>What are your support hours?</b>
+			We have support staff answering emails 24/7, though response
+			times may be a bit slower on weekends.
+		</li>
+		<li>
+			<b>How do I contact support?</b>
+			Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+		</li>
+	</ul>`)
+}
+
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Page Not Found", http.StatusNotFound)
 }
 
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	// fmt.Fprint(w, r.URL.Path) // ability to get the path off the request
-// 	// fmt.Fprint(w, r.URL.Path)
-// 	// fmt.Fprint(w, r.URL.RawPath)
-
-// 	// use a switch instead
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		notFoundHandler(w, r)
-// 	}
-// }
-
 type Router struct {}
 
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprint(w, r.URL.Path) // ability to get the path off the request
-	// fmt.Fprint(w, r.URL.Path)
-	// fmt.Fprint(w, r.URL.RawPath)
-
-	// use a switch instead of if else if
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		notFoundHandler(w, r)
 	}
@@ -55,13 +56,6 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	var router Router
-	// var router http.HandlerFunc = pathHandler
-	// Why does Go allow both types and conversion, makes life easier for us to not have to convert over.
-	// http.Handler - interface with the ServeHTTP method
-	// http.HandlerFunc - function that accepts same args as ServeHTTP method, but implements http.Handler
-	// http.Handle - function that takes in a pattern and takes in an http.Handler
-	// http.HandleFunc - takes in a pattern and takes in a function that has underlying type looks like http.handlerFunc
-	// http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
 	fmt.Println("Starting server on http://localhost:3000...")
 	http.ListenAndServe(":3000", router)
 }
